181349241ecf8885b8c6cb1ef08eb73842a96b3e
luokass7 <72557596+luokass7@users.noreply.github.com>
2021-07-05 15:54:55 +0530

    Add files via upload

---
+++ React/AmazonClone/.js/Header.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <div className='header'>
+            <Link to='/'>
+            <img 
+                className='header__logo'    
+                src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
+            />    
+            </Link>
+            
+            <div className='header__search'>
+                <input
+                className='header__searchInput'
+                type='text' />
+
+                <SearchIcon className='header__searchIcon' />
+            </div>
+                {/* Logo */}
+
+            <div className='header__nav'>
+                <div className='header__option'>
+                    <span className='header__option__line1'>
+                        Hello Guest
+                    </span>
+                    <span className='header__option__line2'>
+                        Sign in
+                    </span>
+                </div>
+
+                <div className='header__option'>
+                    <span className='header__option__line1'>
+                        Returns
+                    </span>
+                    
+                    <span className='header__option__line2'>
+                        & Orders
+                    </span>                   
+                </div>
+
+                <div className='header__option'>
+                    <span className='header__option__line1'>
+                        Your
+                    </span>
+                   
+                    <span className='header__option__line2'>
+                        Prime
+                    </span>                    
+                </div>
+
+                <Link to='/checkout'>
+                <div className='header__optionBasket'>
+                <ShoppingBasketIcon />
+                <span className='header__option__line2 header__basketCount'>
+                    0
+                </span>
+                </div>
+                </Link>
+            </div>
+
+            
+            
+        </div>
+    )
+}
+
+export default Header
---
+++ React/AmazonClone/.js/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD15dE1U4XhFObfTGQxSKfGGkfHWZE1GH8",
+    authDomain: "clone-9a385.firebaseapp.com",
+    projectId: "clone-9a385",
+    storageBucket: "clone-9a385.appspot.com",
+    messagingSenderId: "1067836556098",
+    appId: "1:1067836556098:web:f37a05ebe0b5e89d3bfedb"
+  };
